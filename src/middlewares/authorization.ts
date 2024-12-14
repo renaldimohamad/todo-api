2f55005e2b39cacb6eadda29c44a92a2949ace00
Renaldi Mohamad <sundusardian@gmail.com>
2024-12-14 20:21:21 +0700

    chore(add authorization)

---
+++ src/middlewares/authorization.ts
@@ -0,0 +1,23 @@
+import {NextFunction, Request, Response} from "express"
+import jwt from "jsonwebtoken"
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+   const token = req.headers.authorization?.split(" ")[1]
+
+   if (!token) {
+      res.status(401).json({message: "Unauthorized"})
+      return
+   }
+
+   console.log(token)
+
+   const payload = jwt.verify(token, process.env.SECRET_KEY || "secret")
+
+   if (!payload) {
+      res.status(401).json({message: "Unauthorized"})
+      return
+   }
+
+   res.locals.user = payload
+   next()
+}
