4ddf0b9817000f434ca2675a74a4c663de74993c
Renaldi Mohamad <sundusardian@gmail.com>
2024-12-14 20:23:16 +0700

    chore(add file-uploads)

--- src/middlewares/fileUpload.ts
+++ src/middlewares/fileUpload.ts
@@ -1,20 +1,16 @@
-// import multer from "multer"
-// import path from "path"
+import multer from "multer"
+import path from "path"
 
-// // multer library untuk middelware upload file
-// const storage = multer.diskStorage({
-//    destination: function (req, file, cb) {
-//       cb(null, path.join(__dirname, "..", "/uploads"))
-//    },
-//    filename: function (req, file, cb) {
-//       // if (file.fieldname.split(".")[1].match() ) {
+const storage = multer.diskStorage({
+   destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, "..", "/uploads"))
+   },
+   filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + "-"
+      cb(null, uniqueSuffix + file.originalname)
+   },
+})
 
-//       // }
-//       const uniqueSuffix = Date.now() + "-"
-//       cb(null, uniqueSuffix + file.originalname)
-//    },
-// })
+const upload = multer({storage: storage})
 
-// const upload = multer({storage: storage})
-
-// export default upload
+export default upload
