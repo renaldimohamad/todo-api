149e8bb589ff0cf185e2b02d6b905d37cbb758bd
Renaldi Mohamad <sundusardian@gmail.com>
2024-12-17 17:03:33 +0700

    fix(authorization/middlewares)

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -5,8 +5,9 @@ export default async (req: Request, res: Response, next: NextFunction) => {
    const token = req.headers.authorization?.split(" ")[1]
 
    if (!token) {
-      res.status(401).json({message: "Unauthorized"})
-      return
+       res.status(401).json({message: "Unauthorized"})
+       
+       return
    }
 
    console.log(token)
