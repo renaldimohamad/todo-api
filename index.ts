2ae91adf7671aeae582fddf415074c7879747c4c
Renaldi Mohamad <sundusardian@gmail.com>
2024-12-17 17:02:44 +0700

    chore(fix route)

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ import cors from "cors"
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 2000
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
